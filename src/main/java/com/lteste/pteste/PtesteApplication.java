9d709d3e1d080d5c2811a271b8d5d62dd28464e4
LckzUp <lucas.pinto17@escola.pr.gov.br>
2025-06-27 10:35:31 -0300

    Primeiro Commit da Revisao dos cmd do git

---
+++ src/main/java/com/lteste/pteste/PtesteApplication.java
@@ -0,0 +1,13 @@
+package com.lteste.pteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lteste/pteste/PtesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lteste.pteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
