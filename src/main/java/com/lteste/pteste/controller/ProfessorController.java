50821ddc5596b20887388646ffdb8dcce115c21b
LckzUp <lucas.pinto17@escola.pr.gov.br>
2025-07-08 07:45:25 -0300

    Codigo insert para o usuario

--- src/main/java/com/lteste/pteste/controller/ProfessorController.java
+++ src/main/java/com/lteste/pteste/controller/ProfessorController.java
@@ -1,6 +1,7 @@
 package com.lteste.pteste.controller;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriBuilder;
 
 import com.lteste.pteste.dto.ProfessorDto;
 import com.lteste.pteste.modelo.Professor;
@@ -34,6 +34,13 @@ public class ProfessorController {
     public String ola() { //String(Texto) -> Retona na tela para o Usuario //Não quer dizer HTML
         return "<h1>Luquetes</h1>"; //return -> Devolve  o retorno para quem chamou
     }
+
+    @GetMapping( value = "/listaprofessor") //Select para o usuario
+    public List<Professor> findAll() {
+        return professorRepository.findAll();
+    }
+
+
 
     @PostMapping(value = "/insert")
     public ResponseEntity<?> insert(@RequestBody ProfessorDto professorDto) {
