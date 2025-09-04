05666c9698015690a86f9eec8c741797775c7846
LckzUp <lucas.pinto17@escola.pr.gov.br>
2025-09-04 08:40:40 -0300

    Deletado para fazer o clone do professor e incluir neste repositorio

--- src/main/java/com/lteste/pteste/repository/MateriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lteste.pteste.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lteste.pteste.modelo.Materia;
-
-public interface MateriaRepository extends JpaRepository<Materia,Long>{
-    
-}
